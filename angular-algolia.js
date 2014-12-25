8c9bc17c6c7ec54d1714f6a3aa323d797fba81ee
PatrickJS <github@gdi2290.com>
2014-12-25 01:19:21 -0800

    refactor: as $AlgoliaProvider

--- angular-algolia.js
+++ angular-algolia.js
@@ -1,17 +1,25 @@
-angular.module('algoliasearch', [])
-.provider('algolia', function() {
-  var provider = this;
-  provider.applicationID = undefined;
-  provider.apiKey = undefined;
-  provider.options = {}
-  provider.resolveDNS = undefined;
-  provider.hosts = undefined;
+!function() {
   
-  provider.$get = ['$injector', '$window', function($injector, $window) {
-     provider.options.angular = {
-       '$injector': $injector
-     }
-    return new $window.AlgoliaSearch(provider.applicationID, provider.apiKey, provider.options, provider.resolveDNS, provider.hosts);
- }];
- 
-});
+  function $AlgoliaProvider() {
+    var provider = this;
+    provider.applicationID = undefined;
+    provider.apiKey = undefined;
+    provider.resolveDNS = undefined;
+    provider.hosts = undefined;
+    provider.options = {};
+    
+    provider.$get = ['$injector', '$window', function($injector, $window) {
+       provider.options.angular = {
+         '$injector': $injector
+       }
+      return new $window.AlgoliaSearch(provider.applicationID, provider.apiKey, provider.options, provider.resolveDNS, provider.hosts);
+   }];
+   
+  }
+
+  angular.module('angular-algolia', [])
+  .provider('$algolia', $AlgoliaProvider)
+  .provider('Algolia', $AlgoliaProvider);
+  
+}();
+
